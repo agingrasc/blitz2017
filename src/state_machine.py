e95d586bdf104fd362606701b7fb9d2371e70cf5
Alexandra Mercier <alexandra.mercier.101@gmail.com>
2017-01-07 08:51:32 -0500

    code de base de mouvement (ne fait que stay)

---
+++ src/state_machine.py
@@ -0,0 +1,44 @@
+class MoveState:
+    def __init__(self):
+        self.current_state = StayState
+
+    def new_state(self, new_state):
+        self.current_state = new_state
+
+    def get_dir(self):
+        raise NotImplementedError
+
+    def move(self):
+        dir = self.current_state.get_dir()
+        return dir
+
+
+class StayState(MoveState):
+    def get_dir(self):
+        # envoie la direction
+        return 'STAY'
+
+
+class NorthState(MoveState):
+    def get_dir(self):
+        # envoie la direction
+        return 'NORTH'
+
+
+
+class SouthState(MoveState):
+    def get_dir(self):
+        # envoie la direction
+        return 'SOUTH'
+
+
+class EastState(MoveState):
+    def get_dir(self):
+        # envoie la direction
+        return 'EAST'
+
+
+class WestState(MoveState):
+    def get_dir(self):
+        # envoie la direction
+        return 'WEST'
