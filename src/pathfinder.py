46d98b1513200e5ab0fc90d99dd6fe6fc1cd875d
MaitreMenard <maxime.menard.1@ulaval.ca>
2017-01-07 11:29:23 -0500

    Fix du retour de position de get_closest

--- src/pathfinder.py
+++ src/pathfinder.py
@@ -24,17 +24,19 @@ class Pathfinder:
         assert len(reference_pos) == 2
 
         minimum_distance = math.inf
-        closest = None
+        closest_object = None
+        closest_position = None
         for position in objects_positions:
             distance = self.get_distance(reference_pos, position)
             if distance < minimum_distance:
                 minimum_distance = distance
-                closest = self.game.board.tiles[position[0]][position[1]]
-                if closest == TAVERN:
-                    closest = TavernTile()
-                closest.pos = position
+                closest_object = self.game.board.tiles[position[0]][position[1]]
+                closest_position = position
+                if closest_object == TAVERN:
+                    closest_object = TavernTile()
+                closest_object.pos = position
 
-        return closest, position
+        return closest_object, closest_position
 
     def get_distance(self, pos1, pos2):
         return math.fabs(pos2[0] - pos1[0]) + math.fabs(pos2[1] - pos1[1])
